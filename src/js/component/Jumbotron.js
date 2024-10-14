84a382a318a3d232462b7be5762dbe3e154a1be3
José Ignacio Puig <ipnachoip@gmail.com>
2024-10-14 13:58:01 +0000

    terminado

---
+++ src/js/component/Jumbotron.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+const Jumbotron = ()=>{
+    
+
+}
+
+export default Jumbotron
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -2,24 +2,54 @@ import React from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
+import Card from "./Card";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
 
 //create your first component
 const Home = () => {
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+		<>
+			<div className="container ">
+				<Navbar />
+			</div>
+
+
+			<div className="container">
+
+				<div className=" container-fluid py-5 border  ">
+					<h1 className="display-5 fw-bold">A Warm Welcome!</h1>
+					<p className="col-md-8 fs-4">Using a series of utilities, you can create this jumbotron, just like the one in previous versions of Bootstrap. Check out the examples below for how you can remix and restyle it to your liking.</p>
+					<button className="btn btn-primary btn-lg" type="button">Call to action!</button>
+				</div>
+
+				<div className="row  d-flex flex-nowrap">
+					<div className="col">
+						<Card title="Tarea" />
+
+					</div>
+					<div className="col">
+						<Card title="Card title" />
+					</div>
+					<div className="col">
+						<Card title="Card title" />
+					</div>
+					<div className="col">
+						<Card title="Card title" />
+					</div>
+
+				</div>
+
+				<div className="container-fluid d-flex bg-dark ">
+					<footer class="expand-lg bg-body-tertiary">
+						<h6 className="alaing-center text-white">Copyrigth © Your Website 2024</h6>
+					</footer>
+				</div >
+
+			</div>
+
+
+		</>
 	);
 };
 
